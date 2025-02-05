d4f3f16f607abbce23f437eab6d3671c3e60476f
Roman Lapacherie <roman.lapacherie@gmail.com>
2025-02-05 22:57:05 +0000

    prettier

--- src/lib/components/login-page.svelte
+++ src/lib/components/login-page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import LoginForm from "$lib/components/login-form.svelte";
+  import LoginForm from '$lib/components/login-form.svelte';
 </script>
 
 <div class="flex h-screen w-full items-center justify-center px-4">
-	<LoginForm />
+  <LoginForm />
 </div>
--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
-	import AppSidebar from '$lib/components/app-sidebar.svelte';
+  import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+  import AppSidebar from '$lib/components/app-sidebar.svelte';
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <Sidebar.Provider>
-	<AppSidebar />
-	<main>
-		<Sidebar.Trigger />
-		{@render children?.()}
-	</main>
+  <AppSidebar />
+  <main>
+    <Sidebar.Trigger />
+    {@render children?.()}
+  </main>
 </Sidebar.Provider>

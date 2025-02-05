de1fc028723dccdd884df3b00cd1c22eb5e14675
Roman Lapacherie <roman.lapacherie@gmail.com>
2025-02-05 22:56:43 +0000

    add login form and page components

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,15 +1,7 @@
 <script lang="ts">
 	import '../app.css';
-	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
-	import AppSidebar from "$lib/components/app-sidebar.svelte";
-   
+
 	let { children } = $props();
-  </script>
-   
-  <Sidebar.Provider>
-	<AppSidebar />
-	<main>
-	  <Sidebar.Trigger />
-	  {@render children?.()}
-	</main>
-  </Sidebar.Provider>
+</script>
+
+{@render children?.()}

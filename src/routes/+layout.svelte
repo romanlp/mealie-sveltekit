0562b00d153d4e380c1da695ccf54b50b1970c98
Roman Lapacherie <roman.lapacherie@gmail.com>
2025-02-05 22:47:08 +0000

    minor

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import '../app.css';
+	import * as Sidebar from "$lib/components/ui/sidebar/index.js";
+	import AppSidebar from "$lib/components/app-sidebar.svelte";
+   
 	let { children } = $props();
-</script>
-
-{@render children()}
+  </script>
+   
+  <Sidebar.Provider>
+	<AppSidebar />
+	<main>
+	  <Sidebar.Trigger />
+	  {@render children?.()}
+	</main>
+  </Sidebar.Provider>

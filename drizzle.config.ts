77d86eafbdff1a4d741c22a1247f7cf1f285bb95
Roman Lapacherie <roman.lapacherie@gmail.com>
2025-02-06 22:28:07 +0000

    setup drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DB) throw new Error('DB is not set');
 
 export default defineConfig({
+  dialect: 'sqlite',
   schema: './src/lib/server/db/schema.ts',
-
+  out: './src/lib/server/db/migrations',
+  driver: 'd1-http',
   dbCredentials: {
-    url: process.env.DB
+    accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+    databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+    token: process.env.CLOUDFLARE_D1_TOKEN!
   },
-
   verbose: true,
-  strict: true,
-  dialect: 'sqlite'
+  strict: true
 });
